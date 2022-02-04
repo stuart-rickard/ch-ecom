5970b79b8e2a11b3090678e90993b9451695c3fb
stuart-rickard <stuart@bau-dev.com>
2022-02-04 15:31:59 -0800

    initial commit

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +10,47 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      // NEEDS WORK
+      type: DataTypes.DECIMAL(2), //CHECK
+      allowNull: false,
+      validate: {
+        isDecimal: true, //CHECK
+      },
+    },
+    stock: {
+      // NEEDS WORK
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10, //CHECK
+      validate: {
+        isNumber: true, //CHECK doesn't it have to be a number due to datatype?  we could check that it's a positive number.
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,13 +1,18 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "id",
+  onDelete: "SET NULL",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "id",
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
